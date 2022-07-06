1d722a7fc0886a45f831600ecd45a726c8a760be
GaoyuRain <GAOYU0829>
2022-07-06 10:12:42 +0800

    添加注释

--- utilss/kafka_data_test.py
+++ utilss/kafka_data_test.py
@@ -46,22 +46,29 @@ class AlarmConfig:
     apm_prod_config = [apm_prod_message, apm_alarm_topic, prod_server]
 
     @staticmethod
-    def get_config(cls, type, ruleid, value, env=None):
+    def get_config(type, ruleid, value=None, env=None):
+        '''
+        :param type: 消息类型 fn 泛能设备测点规则  iot 泛能离线规则 xz 新智告警规则 apm 监控告警规则
+        :param ruleid: 规则id
+        :param value: 触发告警的值
+        :param env: 环境
+        :return:
+        '''
         if 'fn'.__eq__(type):
             fn_message = MessageUtils.get_fn_message(ruleid, value)
-            fn_config = [fn_message, cls.fn_alarm_topic, cls.bd_test_server]
+            fn_config = [fn_message, AlarmConfig.fn_alarm_topic, AlarmConfig.bd_test_server]
             return fn_config
         elif 'iot'.__eq__(type):
             fn_iot_message = MessageUtils.get_iot_message(ruleid)
-            fn_iot_config = [fn_iot_message, cls.fn_iot_topic, cls.fn_iot_test_server]
+            fn_iot_config = [fn_iot_message, AlarmConfig.fn_iot_topic, AlarmConfig.fn_iot_test_server]
             return fn_iot_config
         elif 'xz'.__eq__(type):
             xz_message = MessageUtils.get_xz_message(1496774521609191424, 105, '154761f6003644e3ade8c4d252116b67')
-            xz_config = [xz_message, cls.xz_alarm_topic, cls.xz_test_server]
+            xz_config = [xz_message, AlarmConfig.xz_alarm_topic, AlarmConfig.xz_test_server]
             return xz_config
         elif 'apm'.__eq__(type):
             apm_message01 = MessageUtils.get_apm_message(1495996855845318656, 101.108)
-            apm_config = [apm_message01, cls.apm_alarm_topic, cls.xz_test_server]
+            apm_config = [apm_message01, AlarmConfig.apm_alarm_topic, AlarmConfig.xz_test_server]
             return apm_config
 
 
@@ -79,8 +86,8 @@ if __name__ == '__main__':
     # send_kfmsg(AlarmConfig.apm_config)
     # send_kfmsg(AlarmConfig.fn_third_config)
     # time.sleep(2)
-    # send_kfmsg(AlarmConfig.fn_config)
-    send_kfmsg(AlarmConfig.get_config())
+    # send_kfmsg(AlarmConfig.get_config('fn', 990976771213672448, 100))
+    send_kfmsg(AlarmConfig.get_config('iot', 993912110836125696))
     # send_kfmsg(AlarmConfig.fn_config03)
     # time.sleep(2)
 
